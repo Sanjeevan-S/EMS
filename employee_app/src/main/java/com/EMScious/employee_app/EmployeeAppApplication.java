6338d2ebc68f80f8a60ee5009806f26fff90e7ae
Sanjeevan <snjvrocks@gmail.com>
2023-01-15 21:38:15 +0530

    changed the file structure

--- employee_app/src/main/java/com/EMScious/employee_app/EmployeeAppApplication.java
+++ employee_app/src/main/java/com/EMScious/employee_app/EmployeeAppApplication.java
@@ -3,7 +3,7 @@ package com.EMScious.employee_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class EmployeeAppApplication {
 
 	
